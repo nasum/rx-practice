909d49502afe57a32000aa446e33593e244b0aa4
nasum <tomato.wonder.life@gmail.com>
2020-03-16 01:17:52 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "RX Test",
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
